5bbd3c6475232b8da3acdd0ff05eaf332c570a21
nsjoquist <nathan@sunetos.com>
2015-07-01 18:25:03 -0400

    Initial Commit

---
+++ gitJobsViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitJobsViewer
+//
+//  Created by NathanWSjoquist on 7/1/15.
+//  Copyright (c) 2015 Nathan Sjoquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
